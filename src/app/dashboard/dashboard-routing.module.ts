a3d224532ef82f33226778364730b210fdf19c98
Jesus Suarez <jsuarezl@everis.com>
2020-04-22 21:24:34 +0200

    Fix

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 import { dashboardRoutes } from './dashboard.routes';
-//import { AuthGuardService } from '../auth/auth-guard.service';
 
 const routes: Routes = [
   { path: '', 
     component: DashboardComponent, 
     children: dashboardRoutes,
-    //canActivate: [ AuthGuardService ] 
   }
 ]
 
