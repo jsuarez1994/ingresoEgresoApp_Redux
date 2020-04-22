0b919c3514c52a849cc9226088a90dd0e1ff8f53
Jesus Suarez <jsuarezl@everis.com>
2020-04-22 16:42:33 +0200

    Obtener Firebase los items.

--- src/app/ingreso-egreso/detalle/detalle.component.ts
+++ src/app/ingreso-egreso/detalle/detalle.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
+import { IngresoEgresoModel } from '../ingreso-egreso.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-detalle',
   templateUrl: './detalle.component.html',
   styles: []
 })
-export class DetalleComponent implements OnInit {
+export class DetalleComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(private store:Store<AppState>) { }
+
+  listItem: IngresoEgresoModel[];
+  subscription: Subscription = new Subscription();
 
   ngOnInit() {
+    this.subscription = this.store.select('ingresoEgreso').subscribe( items => {
+      this.listItem = items.items;
+    })
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
+  }
+
+
+  borrarItem(uid: string) {
+    console.log(uid);
   }
 
 }
--- src/app/ingreso-egreso/ingreso-egreso.service.ts
+++ src/app/ingreso-egreso/ingreso-egreso.service.ts
@@ -2,27 +2,65 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { IngresoEgresoModel } from './ingreso-egreso.model';
 import { AuthService } from '../auth/auth.service';
+import { AppState } from '../app.reducer';
+import { Store } from '@ngrx/store';
+import { filter, map } from 'rxjs/operators';
+import { SetItemsAction } from './ingreso-egreso.actions';
+import { Subscription } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class IngresoEgresoService {
-  constructor(private afDB: AngularFirestore, private authService: AuthService) {}
+  constructor(
+    private afDB: AngularFirestore,
+    private authService: AuthService,
+    private store: Store<AppState>
+  ) {}
+
+  ingresoEgresoListenerSusb: Subscription = new Subscription();
+  ingresoEgresoItemsSusb: Subscription = new Subscription();
+
+  cancelarSubscription() {
+    this.ingresoEgresoItemsSusb.unsubscribe();
+    this.ingresoEgresoListenerSusb.unsubscribe();
+  }
 
   crearIngresoEgreso(ingresoEgreso: IngresoEgresoModel) {
-
     const user = this.authService.getUser();
 
     return this.afDB
       .doc(`${user.uid}/ingreso-egresos`)
-      .collection('items').add({ ...ingresoEgreso });
+      .collection('items')
+      .add({ ...ingresoEgreso });
   }
 
   initIngresoEgresoListener() {
-
-    const user = this.authService.getUser();
-
-    console.log(user.uid);
+    // Controlamos con pipe(...) que si al principio vemos que el auth es nulo, 
+    // no llega al subscribe
+    this.ingresoEgresoListenerSusb=this.store.select('auth')
+    .pipe( filter( auth => auth.user != null) )
+    .subscribe(auth => { this.getAllIngresoEgresoItems(auth.user.uid); });
 
   }
+
+
+  private getAllIngresoEgresoItems( uid: string) {
+    // Indicamos la ruta a la que vamos a obtener los items
+    this.ingresoEgresoItemsSusb=this.afDB.collection(`${ uid }/ingreso-egresos/items`)
+    .snapshotChanges()
+    .pipe( map( docData => {
+            return docData.map( doc => {
+              return {
+                uid: doc.payload.doc.id,
+                ...doc.payload.doc.data()
+              };
+            });
+          })
+    )
+    .subscribe( (coleccion:IngresoEgresoModel[]) => {
+      this.store.dispatch(new SetItemsAction(coleccion));
+    });
+  }
+
 }
